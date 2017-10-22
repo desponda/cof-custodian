2daa13fd2eab3700b27dc2315f60136e5f696314
Daniel Esponda <danielresponda@gmail.com>
2017-10-22 10:49:19 -0400

    fixing code - test circle ci

--- services/custodian.js
+++ services/custodian.js
@@ -4,7 +4,7 @@ var yaml = require('js-yaml');
 var fs = require('fs');
 var path = require('path');
 
-module.exports.execute = function (){
+module.exports.execute = function () {
     //read policies
     fs.readFile(path.join(__dirname, '../policies/first-policy.yml'), function (err, data) {
         if (err) {
