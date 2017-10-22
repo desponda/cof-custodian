b1c791be312ae7e98aaf51621222c6c32b8a6305
mnl564 <daniel.esponda@capitalone.com>
2017-10-22 00:49:10 -0400

    Adding initial code that reads yml data

---
+++ services/custodian.js
@@ -0,0 +1,19 @@
+/*jslint node: true */
+"use strict";
+var yaml = require('js-yaml');
+var fs = require('fs');
+var path = require('path');
+
+module.exports.execute = function () {
+    //read policies
+    fs.readFile(path.join(__dirname, '../policies/first-policy.yml'), function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            yaml.safeLoadAll(data, function (doc) {
+                console.log(doc);
+            });
+        }
+    });
+
+};
