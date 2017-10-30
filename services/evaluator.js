b96772418d82406a8026e84fdceba8566b024acc
mnl564 <daniel.esponda@capitalone.com>
2017-10-29 21:07:27 -0400

    Adding first policy evaluation TODO: Add filters TODO: Message string should be evaluated

--- services/evaluator.js
+++ services/evaluator.js
@@ -1,10 +1,41 @@
 /*jslint node: true */
 "use strict";
-var account = require("./account");
+var nessieAPI = require("./nessieAPI");
+var notifier = require("./mockNotifier");
+var accountService = new nessieAPI.accountService();
+
+function meetsConditions(resource, conditions) {
+    var meetsCondition = true;
+    conditions.forEach(function (condition) {
+        if (condition.type === 'comparison') {
+            if (condition.operator === 'lessThan') {
+                var a = parseFloat(resource[condition.attribute]), b = parseFloat(condition.value);
+                if (a >= b) {
+                    meetsCondition = false;
+                }
+            }
+        }
+    });
+    return meetsCondition;
+}
+
+function executeAction(resource, actions) {
+    actions.forEach(function (action) {
+        if (action.type === 'notify') {
+            notifier.notify(resource[action.attribute], action.message);
+        }
+    });
+}
+
 
 function evaluateCondition(policy) {
-    return function (context) {
-        console.info(context[0] + policy);
+    return function (resources) {
+        var conditions = policy.conditions;
+        resources.forEach(function (resource) {
+            if (meetsConditions(resource, conditions)) {
+                executeAction(resource, policy.actions);
+            }
+        });
     };
 }
 
@@ -12,7 +43,8 @@ module.exports.evaluate = function (policy) {
     console.info("Evaluating policy: " + policy.name);
     //get context
     if (policy.resource === 'account') {
-        account.getAccounts(evaluateCondition(policy));
+        accountService.getAccounts(evaluateCondition(policy));
+        //future: filters
     }
     //evaluate condition
     //execute action
